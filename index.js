7ab8e4b62b4ef0f127028b845a357c25dc593b2d
Mike Spade <mikespadeny@gmail.com>
2022-09-01 16:22:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+/*
+
+JS was created for the web by Brandon Eich in 1995
+Created to make interactive and dynamic elements
+Took 10 days to create while working on Netscape navigator
+ Programming language as opposed to markup language
+ 
+ */
+
+alert("Michael, How are you?")
+
+// incrementing using a for loop. define the start, end, and actions to be taken
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// ^^ here you should see a print out of 0 - 9
+
+// JS has many data types. here are some numeric expressions to test
+
+console.log(2 + 10 - 19 + 4 - 90 + 1)
+console.log(-20 + -19 - (-10) - (-1) + 24)
+console.log((10/5) * 4 - 20)
+console.log(4096 % 12)
+
+// We can also compare values with JS
+
+console.log(43 > 47)
+console.log(12 == 17)
+console.log(3 <= 3)
+console.log(1 != 0)
+
+/*
+ * Programming Quiz: First Expression (2-1)
+ * Write an expression that uses at least three, distinct, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+console.log((3 + 5) * 6 - 6)
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators. (`+`, `-`, `*`, `/`, or `%`)
+ * 3. Your code should not be empty
+ */
+
+
+// this expression equals 4, change it to equal 42
+console.log((1 + 5 - 2) * 11 -2);
